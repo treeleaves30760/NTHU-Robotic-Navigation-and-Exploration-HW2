e136674b0cb0f48395c406654acd76597d0f8f05
treeleaves30760 <treeleaves30760@gmail.com>
2025-03-22 17:23:18 +0800

    feat: finish the pid

--- PathTracking/controller_pid_basic.py
+++ PathTracking/controller_pid_basic.py
@@ -1,8 +1,9 @@
-import sys
-import numpy as np 
-sys.path.append("..")
-import PathTracking.utils as utils
 from PathTracking.controller import Controller
+import PathTracking.utils as utils
+import sys
+import numpy as np
+sys.path.append("..")
+
 
 class ControllerPIDBasic(Controller):
     def __init__(self, kp=0.4, ki=0.0001, kd=0.5):
@@ -12,26 +13,47 @@ class ControllerPIDBasic(Controller):
         self.kd = kd
         self.acc_ep = 0
         self.last_ep = 0
-    
+
     def set_path(self, path):
         super().set_path(path)
         self.acc_ep = 0
         self.last_ep = 0
-    
+
     def feedback(self, info):
         # Check Path
         if self.path is None:
             print("No path !!")
             return None, None
-        
+
         # Extract State
         x, y, dt = info["x"], info["y"], info["dt"]
 
         # Search Nesrest Target
-        min_idx, min_dist = utils.search_nearest(self.path, (x,y))
+        min_idx, min_dist = utils.search_nearest(self.path, (x, y))
         target = self.path[min_idx]
-        
-        # TODO: PID Control for Basic Kinematic Model 
-        next_w = 0
-        return next_w, target
 
+        # TODO: PID Control for Basic Kinematic Model
+        # Calculate cross track error
+        yaw = info["yaw"]
+
+        # Vector from car to target
+        vec_target = np.array([target[0] - x, target[1] - y])
+        # Normal vector of car's heading
+        vec_normal = np.array(
+            [np.cos(np.deg2rad(yaw + 90)), np.sin(np.deg2rad(yaw + 90))])
+        # Cross track error (lateral deviation)
+        cross_track_error = np.dot(vec_target, vec_normal)
+
+        # PID control
+        self.acc_ep += cross_track_error * dt
+        de = (cross_track_error - self.last_ep) / dt if dt > 0 else 0
+        self.last_ep = cross_track_error
+
+        # Calculate angular velocity based on PID output
+        # Positive when need to turn left
+        next_w = self.kp * cross_track_error + self.ki * self.acc_ep + self.kd * de
+
+        # Normalize angular velocity
+        next_w = utils.angle_norm(next_w)
+
+        return next_w, target
--- PathTracking/controller_pid_bicycle.py
+++ PathTracking/controller_pid_bicycle.py
@@ -1,8 +1,9 @@
-import sys
-import numpy as np 
-sys.path.append("..")
-import PathTracking.utils as utils
 from PathTracking.controller import Controller
+import PathTracking.utils as utils
+import sys
+import numpy as np
+sys.path.append("..")
+
 
 class ControllerPIDBicycle(Controller):
     def __init__(self, kp=0.4, ki=0.0001, kd=0.5):
@@ -12,25 +13,53 @@ class ControllerPIDBicycle(Controller):
         self.kd = kd
         self.acc_ep = 0
         self.last_ep = 0
-    
+
     def set_path(self, path):
         super().set_path(path)
         self.acc_ep = 0
         self.last_ep = 0
-    
+
     def feedback(self, info):
         # Check Path
         if self.path is None:
             print("No path !!")
             return None, None
-        
+
         # Extract State
         x, y, dt = info["x"], info["y"], info["dt"]
 
         # Search Nesrest Target
-        min_idx, min_dist = utils.search_nearest(self.path, (x,y))
+        min_idx, min_dist = utils.search_nearest(self.path, (x, y))
         target = self.path[min_idx]
-        
+
         # TODO: PID Control for Bicycle Kinematic Model
-        next_delta = 0
+        # Calculate cross track error
+        yaw = info["yaw"]
+        current_x, current_y = x, y
+        target_x, target_y = target[0], target[1]
+
+        # Convert to front axle position if we need to
+        if "l" in info:
+            front_x = current_x + info["l"] * np.cos(np.deg2rad(yaw))
+            front_y = current_y + info["l"] * np.sin(np.deg2rad(yaw))
+            current_x, current_y = front_x, front_y
+
+        # Vector from car to target
+        vec_target = np.array([target_x - current_x, target_y - current_y])
+        # Normal vector of car's heading
+        vec_normal = np.array(
+            [np.cos(np.deg2rad(yaw + 90)), np.sin(np.deg2rad(yaw + 90))])
+        # Cross track error
+        cross_track_error = np.dot(vec_target, vec_normal)
+
+        # PID control
+        self.acc_ep += cross_track_error * dt
+        de = (cross_track_error - self.last_ep) / dt if dt > 0 else 0
+        self.last_ep = cross_track_error
+
+        # Calculate steering angle - positive when need to turn left
+        delta = self.kp * cross_track_error + self.ki * self.acc_ep + self.kd * de
+
+        # Normalize steering angle
+        next_delta = utils.angle_norm(delta)
         return next_delta, target
