f6e49b97f14becd0698b9095e75deeeb209964e4
treeleaves30760 <treeleaves30760@gmail.com>
2025-03-23 16:12:13 +0800

    feat: finish the navigation

--- navigation.py
+++ navigation.py
@@ -19,29 +19,34 @@ set_controller_path = False
 # Navigation
 ##############################
 # Mouse Click Callback
+
+
 def mouse_click(event, x, y, flags, param):
     global pose, nav_pos, way_points, path, m_cspace, set_controller_path
     if event == cv2.EVENT_LBUTTONUP:
         nav_pos_new = (x, m.shape[0]-y)
         if m_cspace[nav_pos_new[1], nav_pos_new[0]] > 0.5:
-            way_points = planner.planning((pose[0],pose[1]), nav_pos_new, 20)
+            way_points = planner.planning((pose[0], pose[1]), nav_pos_new, 20)
             if len(way_points) > 1:
                 nav_pos = nav_pos_new
                 path = np.array(cubic_spline_2d(way_points, interval=4))
                 set_controller_path = True
 
+
 def pos_int(p):
     return (int(p[0]), int(p[1]))
 
+
 def render_path(img, nav_pos, way_points, path):
-    cv2.circle(img,nav_pos,5,(0.5,0.5,1.0),3)
+    cv2.circle(img, nav_pos, 5, (0.5, 0.5, 1.0), 3)
     for i in range(len(way_points)):    # Draw Way Points
-        cv2.circle(img, pos_int(way_points[i]), 3, (1.0,0.4,0.4), 1)
+        cv2.circle(img, pos_int(way_points[i]), 3, (1.0, 0.4, 0.4), 1)
     for i in range(len(path)-1):    # Draw Interpolating Curve
-        cv2.line(img, pos_int(path[i]), pos_int(path[i+1]), (1.0,0.4,0.4), 1)
+        cv2.line(img, pos_int(path[i]), pos_int(path[i+1]), (1.0, 0.4, 0.4), 1)
     return img
 
-def navigation(args, simulator, controller, planner, start_pose=(100,200,0)):
+
+def navigation(args, simulator, controller, planner, start_pose=(100, 200, 0)):
     global pose, nav_pos, way_points, path, set_controller_path
     # Initialize
     window_name = "Known Map Navigation Demo"
@@ -52,12 +57,12 @@ def navigation(args, simulator, controller, planner, start_pose=(100,200,0)):
     pose = start_pose
     collision_count = 0
     # Main Loop
-    while(True):
+    while (True):
         # Update State
         simulator.step(command)
         pose = (simulator.state.x, simulator.state.y, simulator.state.yaw)
         print("\r", simulator, "| Goal:", nav_pos, end="\t")
-        
+
         if set_controller_path:
             controller.set_path(path)
             set_controller_path = False
@@ -65,19 +70,52 @@ def navigation(args, simulator, controller, planner, start_pose=(100,200,0)):
         if path is not None and collision_count == 0:
             # TODO: Planning and Controlling
             if args.simulator == "basic":
-                next_v = 0
-                next_w = 0
+                # Get control from controller
+                info = {
+                    "x": simulator.state.x,
+                    "y": simulator.state.y,
+                    "yaw": simulator.state.yaw,
+                    "v": simulator.state.v,
+                    "w": simulator.state.w,
+                    "dt": simulator.dt
+                }
+                next_w, target = controller.feedback(info)
+                next_v = 5.0  # constant velocity
                 command = ControlState("basic", next_v, next_w)
             elif args.simulator == "diff_drive":
-                next_lw = 0
-                next_rw = 0
+                # Get control from controller
+                info = {
+                    "x": simulator.state.x,
+                    "y": simulator.state.y,
+                    "yaw": simulator.state.yaw,
+                    "v": simulator.state.v,
+                    "w": simulator.state.w,
+                    "dt": simulator.dt
+                }
+                next_w, target = controller.feedback(info)
+                next_v = 5.0  # constant velocity
+                # Convert to differential drive control
+                wheel_base = 15.0  # approximate wheel base
+                next_lw = (next_v - wheel_base * next_w / 2)
+                next_rw = (next_v + wheel_base * next_w / 2)
                 command = ControlState("diff_drive", next_lw, next_rw)
             elif args.simulator == "bicycle":
-                next_a = 0
-                next_delta = 0
+                # Get control from controller
+                info = {
+                    "x": simulator.state.x,
+                    "y": simulator.state.y,
+                    "yaw": simulator.state.yaw,
+                    "v": simulator.state.v,
+                    "w": simulator.state.w,
+                    "delta": simulator.cstate.delta,
+                    "l": simulator.l,
+                    "dt": simulator.dt
+                }
+                next_delta, target = controller.feedback(info)
+                next_a = 1.0 if simulator.state.v < 10.0 else 0.0  # simple acceleration control
                 command = ControlState("bicycle", next_a, next_delta)
             else:
-                exit()            
+                exit()
         else:
             command = None
 
@@ -87,8 +125,21 @@ def navigation(args, simulator, controller, planner, start_pose=(100,200,0)):
             collision_count = 1
         if collision_count > 0:
             # TODO: Collision Handling
-            pass
-        
+            # Go back based on the simulator
+            if args.simulator == "basic":
+                command = ControlState("basic", -5.0, 0)
+            elif args.simulator == "diff_drive":
+                command = ControlState("diff_drive", -5.0, -5.0)
+            elif args.simulator == "bicycle":
+                command = ControlState("bicycle", -5.0, -5.0)
+            # Replan the path
+            way_points = planner.planning((pose[0], pose[1]), nav_pos, 20)
+            if len(way_points) > 1:
+                nav_pos = way_points[1]
+                path = np.array(cubic_spline_2d(way_points, interval=4))
+                set_controller_path = True
+                collision_count = 0
+
         # Render Path
         img = simulator.render()
         if nav_pos is not None and way_points is not None:
@@ -103,30 +154,36 @@ def navigation(args, simulator, controller, planner, start_pose=(100,200,0)):
             print()
             break
 
+
 if __name__ == "__main__":
     # Argument Parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--simulator", type=str, default="diff_drive", help="diff_drive/bicycle")
-    parser.add_argument("-c", "--controller", type=str, default="pure_pursuit", help="pid/pure_pursuit/stanley/lqr")
-    parser.add_argument("-p", "--planner", type=str, default="a_star", help="a_star/rrt/rrt_star")
-    parser.add_argument("-m", "--map", type=str, default="Maps/map1.png", help="image file name")
+    parser.add_argument("-s", "--simulator", type=str,
+                        default="diff_drive", help="diff_drive/bicycle")
+    parser.add_argument("-c", "--controller", type=str,
+                        default="pure_pursuit", help="pid/pure_pursuit/stanley/lqr")
+    parser.add_argument("-p", "--planner", type=str,
+                        default="a_star", help="a_star/rrt/rrt_star")
+    parser.add_argument("-m", "--map", type=str,
+                        default="Maps/map1.png", help="image file name")
     args = parser.parse_args()
 
     # Read Map
-    img = cv2.flip(cv2.imread(args.map),0)
-    img[img>128] = 255
-    img[img<=128] = 0
+    img = cv2.flip(cv2.imread(args.map), 0)
+    img[img > 128] = 255
+    img[img <= 128] = 0
     m = np.asarray(img)
     m = cv2.cvtColor(m, cv2.COLOR_RGB2GRAY)
     m = m.astype(float) / 255.
-    m_cspace = 1-cv2.dilate(1-m, np.ones((40,40))) # Configuration-Space
+    m_cspace = 1-cv2.dilate(1-m, np.ones((40, 40)))  # Configuration-Space
 
     # Select Simulator, Controller, and Planner
     try:
         # Simulator / Controller
         if args.simulator == "basic":
             from Simulation.simulator_basic import SimulatorBasic
-            simulator = SimulatorMap(SimulatorBasic, m=m, l=9, wu=7, wv=3, car_w=16, car_f=13, car_r=7)
+            simulator = SimulatorMap(
+                SimulatorBasic, m=m, l=9, wu=7, wv=3, car_w=16, car_f=13, car_r=7)
             if args.controller == "pid":
                 from PathTracking.controller_pid_basic import ControllerPIDBasic as Controller
                 controller = Controller()
@@ -140,7 +197,8 @@ if __name__ == "__main__":
                 raise NameError("Unknown controller!!")
         elif args.simulator == "diff_drive":
             from Simulation.simulator_differential_drive import SimulatorDifferentialDrive
-            simulator = SimulatorMap(SimulatorDifferentialDrive, m=m, l=9, wu=7, wv=3, car_w=16, car_f=13, car_r=7)
+            simulator = SimulatorMap(
+                SimulatorDifferentialDrive, m=m, l=9, wu=7, wv=3, car_w=16, car_f=13, car_r=7)
             if args.controller == "pid":
                 from PathTracking.controller_pid_basic import ControllerPIDBasic as Controller
                 controller = Controller()
@@ -153,8 +211,9 @@ if __name__ == "__main__":
             else:
                 raise NameError("Unknown controller!!")
         elif args.simulator == "bicycle":
-            from Simulation.simulator_bicycle import SimulatorBicycle 
-            simulator = SimulatorMap(SimulatorBicycle, m=m, l=20, d=5, wu=5, wv=2, car_w=14, car_f=25, car_r=5)
+            from Simulation.simulator_bicycle import SimulatorBicycle
+            simulator = SimulatorMap(
+                SimulatorBicycle, m=m, l=20, d=5, wu=5, wv=2, car_w=14, car_f=25, car_r=5)
             if args.controller == "pid":
                 from PathTracking.controller_pid_bicycle import ControllerPIDBicycle as Controller
                 controller = Controller()
@@ -184,5 +243,5 @@ if __name__ == "__main__":
         planner = Planner(m_cspace)
     except:
         raise
-    
+
     navigation(args, simulator, controller, planner)
